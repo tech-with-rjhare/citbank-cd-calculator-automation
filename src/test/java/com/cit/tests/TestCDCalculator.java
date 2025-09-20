354b3ae16e968b264639857a943e75a0a9b29197
Rakesh <rakeshjhare24@gmail.com>
2025-09-20 16:05:37 +0530

    add new test method & parallel testing, serial testing

--- src/test/java/com/cit/tests/TestCDCalculator.java
+++ src/test/java/com/cit/tests/TestCDCalculator.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -27,16 +28,10 @@ public class TestCDCalculator {
     private WebElement depAmount;
     private WebElement months;
     private WebElement interest;
-    private String url = "https://www.cit.com/cit-bank/resources/calculators/certificate-of-deposit-calculator";
 
     @BeforeClass
     void setup(){
-        driver = new ChromeDriver();
-        expWait = new WebDriverWait(driver, Duration.ofSeconds(10));
         softAssert = new SoftAssert();
-        driver.get(url);
-        driver.manage().window().maximize();
-
     }
 
     void acceptCookies(){
@@ -46,23 +41,36 @@ public class TestCDCalculator {
             System.out.println("Cookies accepted.");
     }
 
-   /* @Test(priority = 0)
-     void testTitleOfPage(){
+    @BeforeMethod
+    @Parameters({"browser","aut_url"})
+    void launchApplication(String br, String url){
+
+        switch (br.toLowerCase()){
+            case "edge": driver = new EdgeDriver(); break;
+            case "chrome": driver = new ChromeDriver();break;
+            default: System.out.println("Invalid browser");return;
+        }
+        expWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get(url);
+        driver.manage().window().maximize();
         acceptCookies();
+    }
+
+    @Test()
+     void testTitleOfPage(){
         String titleOfPage = "CD Calculator | Certificate of Deposit Calculator | CIT Bank";
-        expWait.until(ExpectedConditions.titleContains(titleOfPage));
+        expWait.until(ExpectedConditions.titleContains("CD Calculator"));
         softAssert.assertEquals(titleOfPage,driver.getTitle());
         System.out.println("Title of page : "+driver.getTitle());
         softAssert.assertAll();
-        driver.close();
-    } */
+    }
 
-    @Test()
-    void testCIDaily() throws Exception {
-        acceptCookies();
+    @Test(priority = 1,dependsOnMethods = {"testTitleOfPage"})
+    @Parameters({"browser"})
+    void testCIDaily(String bro) throws Exception {
         String xlFilePath = System.getProperty("user.dir") + "\\src\\main\\resources\\testdata_cdcalculator.xlsx";
         int rowCount = ExcelUtils.getRowCount(xlFilePath, "Compounded_Daily");
-        System.out.println("Row : " + rowCount);
+        //System.out.println("Row : " + rowCount);
         //ExcelUtils.getrowdata(xlFilePath,"Compounded_Daily");
         //acceptCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
@@ -73,11 +81,9 @@ public class TestCDCalculator {
         for(int row=1; row<rowCount;row++){
 
         depositAmount = ExcelUtils.getCellData(xlFilePath, "Compounded_Daily", row, 0);
-        System.out.println(depositAmount);
         lengthOfCD = ExcelUtils.getCellData(xlFilePath, "Compounded_Daily", row, 1);
-        System.out.println(lengthOfCD);
         interestRate = ExcelUtils.getCellData(xlFilePath, "Compounded_Daily", row, 2);
-        System.out.println(interestRate);
+        System.out.println(depositAmount +" | "+lengthOfCD+" | "+interestRate);
 
 
         depAmount.clear();
@@ -108,23 +114,77 @@ public class TestCDCalculator {
         String expectedRes = ExcelUtils.getCellData(xlFilePath, "Compounded_Daily", row, 4);
         expWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@id='displayTotalValue']")));
         String actualRes = driver.findElement(By.xpath("//span[@id='displayTotalValue']")).getText();
-        System.out.println("Actual result : " + actualRes + "\n" + "expectedRes : " + expectedRes);
-        if (actualRes.equals(expectedRes)) {
-            ExcelUtils.setCellData(xlFilePath, "Compounded_Daily", row, 6, "Passed");
-            ExcelUtils.fillGreenColor(xlFilePath, "Compounded_Daily", row, 6);
-        }else
-        {
-            ExcelUtils.setCellData(xlFilePath, "Compounded_Daily", row, 6, "Failed");
-            ExcelUtils.fillRedColor(xlFilePath, "Compounded_Daily", row, 6);
+        System.out.println("Actual result : " + actualRes + " " + "expectedRes : " + expectedRes);
+        if(bro.equals("chrome")) {
+            if (actualRes.equals(expectedRes)) {
+                ExcelUtils.setCellData(xlFilePath, "Compounded_Daily", row, 6, "Passed");
+                ExcelUtils.fillGreenColor(xlFilePath, "Compounded_Daily", row, 6);
+            } else {
+                ExcelUtils.setCellData(xlFilePath, "Compounded_Daily", row, 6, "Failed");
+                ExcelUtils.fillRedColor(xlFilePath, "Compounded_Daily", row, 6);
+            }
         }
 
     }
-        //Assert.assertEquals(expectedRes, actualRes,"Calculation logic doesn't match with requirement");
+            //Assert.assertEquals(expectedRes, actualRes,"Calculation logic doesn't match with requirement");
 
     }
 
-    @AfterClass
+    @Test(priority = 2,dependsOnMethods = {"testTitleOfPage"})
+    void testCIMonthly() throws Exception {
+        String xlFilePath = System.getProperty("user.dir") + "\\src\\main\\resources\\testdata_cdcalculator.xlsx";
+        int rowCount = ExcelUtils.getRowCount(xlFilePath, "Compounded_Monthly");
+        System.out.println("Row : " + rowCount);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        depAmount = driver.findElement(By.xpath("//input[@id='mat-input-0']"));
+        months = driver.findElement(By.xpath("//input[@id='mat-input-1']"));
+        interest = driver.findElement(By.xpath("//input[@id='mat-input-2']"));
+
+        for(int row=1; row<rowCount;row++){
+
+            depositAmount = ExcelUtils.getCellData(xlFilePath, "Compounded_Monthly", row, 0);
+            lengthOfCD = ExcelUtils.getCellData(xlFilePath, "Compounded_Monthly", row, 1);
+            interestRate = ExcelUtils.getCellData(xlFilePath, "Compounded_Monthly", row, 2);
+            System.out.println(depositAmount +" | "+lengthOfCD+" | "+interestRate);
+
+
+            depAmount.clear();
+            months.clear();
+            interest.clear();
+
+            depAmount.sendKeys(depositAmount);
+            months.sendKeys(lengthOfCD);
+            interest.sendKeys(interestRate);
+
+            WebElement dropdownCompound = driver.findElement(By.xpath("//mat-select[@id='mat-select-0']"));
+            dropdownCompound.click();
+
+            WebElement daily = expWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//mat-option//span[contains(text(),'Compounded Monthly')]")));
+            daily.click();
+
+            driver.findElement(By.xpath("//button[@id='CIT-chart-submit']")).click();
+            String expectedRes = ExcelUtils.getCellData(xlFilePath, "Compounded_Monthly", row, 4);
+            expWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[@id='displayTotalValue']")));
+            String actualRes = driver.findElement(By.xpath("//span[@id='displayTotalValue']")).getText();
+            System.out.println("Actual result : " + actualRes + "\n" + "expectedRes : " + expectedRes);
+            /*if (actualRes.equals(expectedRes)) {
+                ExcelUtils.setCellData(xlFilePath, "Compounded_Monthly", row, 6, "Passed");
+                ExcelUtils.fillGreenColor(xlFilePath, "Compounded_Monthly", row, 6);
+            }else
+            {
+                ExcelUtils.setCellData(xlFilePath, "Compounded_Monthly", row, 6, "Failed");
+                ExcelUtils.fillRedColor(xlFilePath, "Compounded_Monthly", row, 6);
+            }*/
+
+        }
+
+    }
+
+    @AfterMethod
     void tearDown(){
-        driver.quit();
+        if (driver != null) {
+            driver.quit();   // ✅ Har test ke baad browser close
+        }
     }
+
 }
